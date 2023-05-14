aa6b8fc8bc4b71aa1b2a2b1961a3052d412a1859
pengkunbin <815371297@qq.com>
2023-05-14 20:55:58 +0800

    feat(texture): 提交纹理示例

--- texture/main.cpp
+++ texture/main.cpp
@@ -28,9 +28,15 @@ void initTriangleShader(unsigned int &shaderProgram, unsigned int &VAO)
     // 编译着色器
     const char *vertexShaderSource = "#version 330 core\n"
                                      "layout (location = 0) in vec3 aPos;\n"
+                                     "layout (location = 1) in vec3 aColor;\n"
+                                     "layout (location = 2) in vec2 aTexCoord;\n"
+                                     "out vec3 ourColor;"
+                                     "out vec2 TexCoord;"
                                      "void main()\n"
                                      "{\n"
                                      "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                     "   ourColor = aColor;"
+                                     "   TexCoord = vec2(aTexCoord.x, aTexCoord.y);;"
                                      "}\0";
     unsigned int vertexShader;
     vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -40,9 +46,12 @@ void initTriangleShader(unsigned int &shaderProgram, unsigned int &VAO)
     // 片段着色器
     const char *fragmentShaderSource = "#version 330 core\n"
                                        "out vec4 FragColor;\n"
+                                       "in vec3 ourColor;\n"
+                                       "in vec2 TexCoord;\n"
+                                       "uniform sampler2D ourTexture;\n"
                                        "void main()\n"
                                        "{\n"
-                                       "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                       "   FragColor = texture(ourTexture, TexCoord);\n"
                                        "}\0";
     unsigned int fragmentShader;
     fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
@@ -66,24 +75,44 @@ void initTriangleShader(unsigned int &shaderProgram, unsigned int &VAO)
 
     // 顶点着色器
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f, 0.5f, 0.0f
+        //     ---- 位置 ----       ---- 颜色 ----     - 纹理坐标 -
+        0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,   // 右上
+        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,  // 右下
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // 左下
+        -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f   // 左上
+    };
+
+    unsigned int indices[] = {
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
     };
 
     // 生成VBO对象
-    unsigned int VBO;
+    unsigned int VBO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
     glBindVertexArray(VAO);
 
     // 将顶点数据复制到VBO中
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     // 指定顶点属性的格式和位置
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
+
+    // 指定片段属性的格式和位置
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    // 指定纹理属性的格式和位置
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
 
     // 解绑VBO和VAO对象
     glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -91,11 +120,33 @@ void initTriangleShader(unsigned int &shaderProgram, unsigned int &VAO)
 }
 
 // 加载纹理
-void initTexture(unsigned int texture) {
-    int width, height, nrChannels;
-    unsigned char *data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
-    
+void initTexture(unsigned int texture)
+{
     glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    // 为当前绑定的纹理对象设置环绕、过滤方式
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    // TODO：这里使用jpg格式的图片渲染时成黑白且倾斜，没搞懂是什么原因
+    int width, height, nrChannels;
+    stbi_set_flip_vertically_on_load(true);
+    unsigned char *data = stbi_load("../img/mipmaps.png", &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        GLenum format = (nrChannels == 3) ? GL_RGB : GL_RGBA;
+
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+
+    stbi_image_free(data);
 }
 
 int main()
@@ -151,10 +202,13 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        // 绑定纹理
+        glBindTexture(GL_TEXTURE_2D, texture);
+
         // 渲染三角形
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         // 检查并调用事件，交换缓冲
         glfwSwapBuffers(window);
